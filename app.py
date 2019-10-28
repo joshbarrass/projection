2fd19b5f8bbef36cf6289fac9e02ddc152ad935a
WORD559 <josh.barrass.work@gmail.com>
2019-10-28 23:57:59 +0000

    Add centre correction to screen

--- app.py
+++ app.py
@@ -26,17 +26,21 @@ class App(object):
             pygame.display.set_icon(self.icon)
         self.display = pygame.display.set_mode(self.size, pygame.HWSURFACE | DOUBLEBUF)
 
-        self.screen = Screen(self.size, FOV)
+        self.fov = FOV
+        self.screen = Screen(self.size, self.fov, (self.size[0]/2, self.size[1]/2))
         self.cube = Cube(A)
         self.a = self.cube.a
         self.top_left = [self.size[0]/4 - self.a/2, self.size[1]/4 - self.a/2, self.a/2]
         #self.top_left = [self.size[0]/2 - self.a/2, self.size[1]/2 - self.a/2, self.a/2]
-        self.centraliser = [self.size[0]/2, self.size[1]/2, 0]
+        #self.centraliser = [self.size[0]/2, self.size[1]/2, 0]
         self.dir = 0
         self.speed = 100
 
         self.zdir = 0
         self.zspeed = self.a*2
+
+        self.fovdir = 0
+        self.fovspeed = 0
 
         self.lineFunc = pygame.draw.aaline if enableAA else pygame.draw.line
 
@@ -49,39 +53,56 @@ class App(object):
     def __loop__(self):
         """Commands processed every frame"""
         clock_factor = (self.clock.get_time()/1000)
-        to_move = self.speed*clock_factor
-        if self.dir == 0: #going right
-            self.top_left[0] += to_move
-            if self.top_left[0] >= 3*self.size[0]/4 - self.a/2:
-                self.top_left[0] = 3*self.size[0]/4 - self.a/2
-                self.dir = 1
-        elif self.dir == 1: #going down
-            self.top_left[1] += to_move
-            if self.top_left[1] >= 3*self.size[1]/4 - self.a/2:
-                self.top_left[1] = 3*self.size[1]/4 - self.a/2
-                self.dir = 2
-        elif self.dir == 2: #going left
-            self.top_left[0] -= to_move
-            if self.top_left[0] <= self.size[0]/4 - self.a/2:
-                self.top_left[0] = self.size[0]/4 - self.a/2
-                self.dir = 3
-        elif self.dir == 3:
-            self.top_left[1] -= to_move
-            if self.top_left[1] <= self.size[1]/4 - self.a/2:
-                self.top_left[1] = self.size[1]/4 - self.a/2
-                self.dir = 0
 
-        to_zmove = self.zspeed*clock_factor
-        if self.zdir == 0: # going back
-            self.top_left[2] += to_zmove
-            if self.top_left[2] >= 10*self.a:
-                self.top_left[2] = 10*self.a
-                self.zdir = 1
-        elif self.zdir == 1: # going forward
-            self.top_left[2] -= to_zmove
-            if self.top_left[2] <= -self.a*5:
-                self.top_left[2] = -self.a*5
-                self.zdir = 0
+        if self.speed > 0:
+            to_move = self.speed*clock_factor
+            if self.dir == 0: #going right
+                self.top_left[0] += to_move
+                if self.top_left[0] >= 3*self.size[0]/4 - self.a/2:
+                    self.top_left[0] = 3*self.size[0]/4 - self.a/2
+                    self.dir = 1
+            elif self.dir == 1: #going down
+                self.top_left[1] += to_move
+                if self.top_left[1] >= 3*self.size[1]/4 - self.a/2:
+                    self.top_left[1] = 3*self.size[1]/4 - self.a/2
+                    self.dir = 2
+            elif self.dir == 2: #going left
+                self.top_left[0] -= to_move
+                if self.top_left[0] <= self.size[0]/4 - self.a/2:
+                    self.top_left[0] = self.size[0]/4 - self.a/2
+                    self.dir = 3
+            elif self.dir == 3:
+                self.top_left[1] -= to_move
+                if self.top_left[1] <= self.size[1]/4 - self.a/2:
+                    self.top_left[1] = self.size[1]/4 - self.a/2
+                    self.dir = 0
+                
+        if self.zspeed > 0:
+            to_zmove = self.zspeed*clock_factor
+            if self.zdir == 0: # going back
+                self.top_left[2] += to_zmove
+                if self.top_left[2] >= 10*self.a:
+                    self.top_left[2] = 10*self.a
+                    self.zdir = 1
+            elif self.zdir == 1: # going forward
+                self.top_left[2] -= to_zmove
+                if self.top_left[2] <= -self.a*5:
+                    self.top_left[2] = -self.a*5
+                    self.zdir = 0
+                    
+        if self.fovspeed > 0:
+            to_fovmove = self.fovspeed*clock_factor
+            if self.fovdir == 0: # increasing
+                self.fov += self.fovspeed
+                if self.fov >= 160:
+                    self.fov = 160
+                    self.fovdir = 1
+            if self.fovdir == 1: # decreasing
+                self.fov -= self.fovspeed
+                if self.fov <= 10:
+                    self.fov = 10
+                    self.fovdir = 0
+            self.screen.set_fov(self.fov)
 
     def __events__(self, event):
         """Event Handling"""
@@ -95,7 +116,7 @@ class App(object):
         coords[0] += self.a/2
         coords[1] += self.a/2
         coords[2] += self.a/2
-        cube_points = self.screen.transform_coords(self.cube.vert+self.top_left-self.centraliser)+self.centraliser
+        cube_points = self.screen.transform_coords(self.cube.vert+self.top_left)
         flat_points = cube_points[:,:2]
 
         # draw vertices
@@ -113,7 +134,7 @@ class App(object):
         fps_text = self.small_font.render(f"FPS: {self.clock.get_fps():.1f}", 1, (0,0,0))
         self.display.blit(fps_text, (0,0))
 
-        fov_text = self.small_font.render(f"FOV: {FOV}   a: {self.a}", 1, (0,0,0))
+        fov_text = self.small_font.render(f"FOV: {self.fov}   a: {self.a}", 1, (0,0,0))
         self.display.blit(fov_text, (0,10))
 
         coord_text = self.medium_font.render(f"Position: ({coords[0]:.0f}, {coords[1]:.0f}, {coords[2]:.0f})", 1, (0,0,0))
--- screen.py
+++ screen.py
@@ -7,8 +7,9 @@ except ImportError:
     have_numpy = False
 
 class Screen(object):
-    def __init__(self, size, fov):
+    def __init__(self, size, fov, centre=(0,0)):
         self.size = size
+        self.centre = centre
         self.set_fov(fov)
 
     def set_fov(self, fov):
@@ -19,7 +20,11 @@ class Screen(object):
 
     def transform_coords(self, coords, add=True):
         """coord can be numpy array or any other iterable containing
-3D coordinates"""
+3D coordinates
+
+z coordinate is fairly meaningless after projecting as it should
+always equal Screen.z. If z == 0 after projecting, then the original
+z coordinate was behind the screen."""
         isarray = isinstance(coords, np.ndarray)
         if isarray:
             if len(coords.shape) != 2 or coords.shape[1] != 3:
@@ -29,13 +34,14 @@ class Screen(object):
                 raise TypeError("coords must contain 3D coordiantes")
 
         if isarray:
+            cc = [self.centre[0], self.centre[1], 0] # centre correction
             zeros = np.zeros(coords.shape)
             coords = coords.astype("float64")
             if add:
                 coords[:,2] += self.z
             mask = coords[:,2]>0
             trans = coords[mask]
-            zeros[mask] = trans*(self.z/trans[:,2])[:,None]
+            zeros[mask] = (trans-cc)*(self.z/trans[:,2])[:,None]+cc
             return zeros
         # else:
         for i in range(len(coords)):
@@ -43,4 +49,5 @@ class Screen(object):
                 coords[i][2] += self.z
             if coords[i][2] <= 0:
                 coords[i] = [inf, inf, inf]
-        return [(c[0]*(self.z/c[2]), c[1]*(self.z/c[2]), self.z) if c[2] > 0 else (0,0,0) for c in coords]
+        return [((c[0]-self.centre[0])*(self.z/c[2])+self.centre[0],
+                 (c[1]-self.centre[1])*(self.z/c[2])+self.centre[1], self.z) if c[2] > 0 else (0,0,0) for c in coords]
